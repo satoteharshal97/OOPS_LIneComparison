8dbe569daf38beff8450bf114ea7402ae4213d15
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-31 17:43:17 +0530

    Modified - Methods are modified

--- src/com/example/linecomparison/LineComparison.java
+++ src/com/example/linecomparison/LineComparison.java
@@ -6,40 +6,41 @@ public class LineComparison {
 
     public static void main(String[] args) {
     afterComparingLines();
+
     }
     static void afterComparingLines() {
-        double x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-        double px1 = 0, px2 = 0, py1 = 0, py2 = 0;
-        getPoints(x1, x2, y1, y2);
-        double lenghtOfline1 = getLengthOfLine(x1, x2, y1, y2);
-        System.out.println();
-        getPoints(px1, px2, py1, py2);
-        double lenghtOfLine2 = getLengthOfLine(px1, px2, py1, py2);
-        Double x = lenghtOfline1;
-        Double y = lenghtOfLine2;
+        double X = 0, Y = 0 , X1 = 0, Y1 = 0;
+        double lengthOfline1 = getLengthCompletely(X, Y, X1, Y1);
+        double lengthOfline2 = getLengthCompletely(X, Y, X1, Y1);
+
+        Double x = lengthOfline1;
+        Double y = lengthOfline2;
         compareLines(x, y);
     }
-    static double getLengthOfLine(double px1, double px2, double py1, double py2){
-        return Math.sqrt(Math.pow((px2 - px1), 2) + Math.pow((py2 - py1), 2));
-    }
+
     static void compareLines(Double x, Double y){
-        boolean bool = x.equals(y);
+        Boolean bool = x.equals(y);
         if(bool) {
             System.out.println("Lines are equal");
         }else {
             System.out.println("Lines are distinct");
         }
     }
-    static void getPoints(double X, double X1, double Y, double Y1){
+
+    static double getLengthCompletely(double X, double Y, double X1, double Y1){
         Scanner sc = new Scanner(System.in);
-        //Taking values of both points from user
-        System.out.println("Enter x-cordinate of first point: ");
-        double xa1 = sc.nextDouble();
-        System.out.println("Enter y-cordinate of first point: ");
-        double ya1 = sc.nextDouble();
-        System.out.println("Enter x-cordinate of second point: ");
-        double xa2 = sc.nextDouble();
-        System.out.println("Enter y-cordinate of second point: ");
-        double ya2 = sc.nextDouble();
+        //Taking values of  points from user
+        System.out.println("Enter x-cordinate of first point:");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of first point:");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter x-cordinate of second point:");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of second point:");
+        double y2 = sc.nextDouble();
+
+        //Calulating length of line
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2- y1), 2));
     }
+
 }
