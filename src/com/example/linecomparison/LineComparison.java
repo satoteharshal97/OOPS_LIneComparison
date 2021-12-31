edfbdb26b35380ae98bc0c469c12f6d59774a752
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-31 15:47:13 +0530

    Added - Get line length

--- src/com/example/linecomparison/LineComparison.java
+++ src/com/example/linecomparison/LineComparison.java
@@ -1,7 +1,26 @@
 package com.example.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison");
+        getlength();
+    }
+    static void getlength(){
+        Scanner sc = new Scanner(System.in);
+        //Taking values of both points from user
+        System.out.println("Enter x-cordinate of first point:");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of first point:");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter x-cordinate of second point:");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of second point:");
+        double y2 = sc.nextDouble();
+        sc.close();
+
+        //Calulating length of line
+        double lenghtOfline = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2- y1), 2));
+        System.out.println("Lenght of line : "+ lenghtOfline);
     }
 }
